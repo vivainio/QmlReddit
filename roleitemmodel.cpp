a7b86f0d3ec22d1573bb5c442603bfb63298e172
Ville M. Vainio <ville.ma.vainio@nokia.com>
2010-11-01 19:25:43 +0200

    Proper example to RoleItemModel

--- roleitemmodel.cpp
+++ roleitemmodel.cpp
@@ -1,5 +1,52 @@
 #include "roleitemmodel.h"
 
+
+/* Example usage:
+
+
+Enumerate the role ID's somewhere
+---------------------------------
+
+struct RedditEntry {
+
+    enum RedditRoles {
+        UrlRole = Qt::UserRole + 1,
+        DescRole,
+        ...
+    };
+    ...
+}
+
+Instantiate the class
+---------------------
+
+
+    QHash<int, QByteArray> roleNames;
+    roleNames[RedditEntry::UrlRole] =  "url";
+    roleNames[RedditEntry::ScoreRole] = "score";
+    m_linksmodel = new RoleItemModel(roleNames);
+
+
+
+Populate with data:
+-------------------
+
+    QStandardItem* it = new QStandardItem();
+    it->setData(e.desc, RedditEntry::DescRole);
+    it->setData(e.score, RedditEntry::ScoreRole);
+
+    m_linksmodel->appendRow(it);
+
+Expose to QML:
+-------------
+
+QDeclarativeContext *ctx = ...
+
+ctx->setContextProperty("mdlLinks", m_linksmodel);
+
+*/
+
+
 RoleItemModel::RoleItemModel(const QHash<int, QByteArray> &roleNames)
 {
     setRoleNames(roleNames);
